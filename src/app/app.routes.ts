0c44aeb072b2afe28dfd08ede36051c9929c6b80
rahmaelsayedddd <119257518+rahmaelsayedddd@users.noreply.github.com>
2025-06-10 17:48:28 +0300

    add classes component  with only View, Edit, Delete, Create functionality & Basic styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ClassesComponent } from './Components/classes/classes.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'classes/:id', component: ClassesComponent, title: "Classes"}
+];
